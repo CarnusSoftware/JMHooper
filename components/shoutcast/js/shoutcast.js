641a6ffee1c0445606086c5dd5c0eededa4ef6c3
jennahooper <jmhooper7@gmail.com>
2016-06-13 20:12:23 -0400

    copying files from hits radio for basic layout

---
+++ components/shoutcast/js/shoutcast.js
@@ -0,0 +1,9 @@
+﻿$.SHOUTcast({
+    host: 'http://176.31.248.14',
+    port: 15574,
+    interval: 5000,
+    stats: function () {
+        $('#songtitle').text(this.get('songtitle'));
+    }
+}).startStats();
+
---
+++ js/audioControls.js
@@ -0,0 +1,24 @@
+﻿    // Audio
+    var audio = document.getElementById("internetRadio");
+
+    // Buttons
+    var playButton = document.getElementById("playIt");
+
+
+
+// Event listener for the play/pause button
+playButton.addEventListener("click", function () {
+    if (audio.paused == true) {
+        // Play the video
+        audio.play();
+
+        // Update the button text to 'Pause'
+        playButton.innerHTML = "Pause";
+    } else {
+        // Pause the video
+        audio.pause();
+
+        // Update the button text to 'Play'
+        playButton.innerHTML = "Play";
+    }
+});
