d6cd58f6a2c622e95089f09e6634f16ea730ccb0
Philip Langman <plangman@spscommerce.com>
2017-04-10 23:15:08 -0400

    initial commit

--- components/shoutcast/js/shoutcast.js
+++
@@ -1,9 +0,0 @@
-﻿$.SHOUTcast({
-    host: 'http://176.31.248.14',
-    port: 15574,
-    interval: 5000,
-    stats: function () {
-        $('#songtitle').text(this.get('songtitle'));
-    }
-}).startStats();
-
